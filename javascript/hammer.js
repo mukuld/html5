2ebcba80b56e114dd2dc208edc31dc4cd0c08d92
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2018-01-26 16:10:15 -0800

    Initial commit

---
+++ javascript/hammer.js
@@ -0,0 +1,10 @@
+var price = 15.00; 	//price of the hammer
+var money = 20.00;	//how much money I have
+
+if (money >= price) {
+	console.log("Buy the hammer.");
+}
+else {
+	console.log("Don't buy the hammer.");
+}
+
---
+++ javascript/juliajames_v2.js
@@ -0,0 +1,7 @@
+var x = 1;
+
+while (x <= 100) {
+    var remainder = !(x%3) ? (!(x%5) ?console.log("juliajames") :console.log("julia")):!(x%5) ? console.log("james") : console.log(x);
+    x++;
+}
+
