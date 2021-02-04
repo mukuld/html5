a0dffd62978689ebdee296c3fbf22208cb9ed01e
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2021-02-04 00:58:05 -0800

    same template for all pages.

--- javascript/designs.js
+++ javascript/designs.js
@@ -4,47 +4,47 @@ let colorChoice, hght, wdth;
 //let hght, wdth;
 // When size is submitted by the user, call makeGrid()
 
-function eraseGrid(){
-	$("#clearGrid").click(function(e) {
-		$("#pixelCanvas").empty();
-	});
+function eraseGrid() {
+    $("#clearGrid").click(function(e) {
+        $("#pixelCanvas").empty();
+    });
 };
-		
-$("#sizePicker").on("submit", function(e){
-	e.preventDefault();
-	clearGrid();
-	makeGrid();
-	addColor();
-	});
+
+$("#sizePicker").on("submit", function(e) {
+    e.preventDefault();
+    clearGrid();
+    makeGrid();
+    addColor();
+});
 
 eraseGrid();
 
 function clearGrid() {
-	$("#pixelCanvas").empty();
-	};
+    $("#pixelCanvas").empty();
+};
 
 function makeGrid() {
-	//eraseGrid();
-	hght = $("#inputHeight").val();
-	//console.log("Height is = " + hght);
-	wdth = $("#inputWidth").val();
-	//console.log("Width is " + wdth);
-	for (let r = 0; r < hght; r++) {
-		$("table").append("<tr id = row" + r + "></tr>");
-		//console.log("r = " + r);
-		for (let c = 0; c < wdth; c++) {
-			$("#row" + r).append('<td></td>');
-			//console.log("c = " + c);
-		};
-	};
+    //eraseGrid();
+    hght = $("#inputHeight").val();
+    //console.log("Height is = " + hght);
+    wdth = $("#inputWidth").val();
+    //console.log("Width is " + wdth);
+    for (let r = 0; r < hght; r++) {
+        $("table").append("<tr id = row class='pixel'" + r + "></tr>");
+        //console.log("r = " + r);
+        for (let c = 0; c < wdth; c++) {
+            $("#row" + r).append('<td></td>');
+            //console.log("c = " + c);
+        };
+    };
 };
 
 function addColor() {
-	$("td").click(function() {
-		colorChoice = $("#colorPicker").val()
-		//console.log("colorChoice");
-		$(this).css("background-color", colorChoice);
-})
+    $("td class='pixel'").click(function() {
+        colorChoice = $("#colorPicker").val()
+            //console.log("colorChoice");
+        $(this).css("background-color", colorChoice);
+    })
 };
 
 //cell in row ' + r + ', column ' + c + '
