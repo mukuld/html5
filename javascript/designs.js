ab3d77ac643042d2c139e098bb455e38cd8796b3
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2021-02-23 23:05:25 -0800

    debug

--- javascript/designs.js
+++ javascript/designs.js
@@ -26,15 +26,15 @@ function clearGrid() {
 function makeGrid() {
     //eraseGrid();
     hght = $("#inputHeight").val();
-    console.log("Height is = " + hght);
+    //console.log("Height is = " + hght);
     wdth = $("#inputWidth").val();
-    console.log("Width is " + wdth);
+    //console.log("Width is " + wdth);
     for (let r = 0; r < hght; r++) {
         $("table").append("<tr id='row' class='pixels'></tr>");
-        console.log("r = " + r);
+        //console.log("r = " + r);
         for (let c = 0; c < wdth; c++) {
-            //$("#row").append('<td></td>');
-            console.log("c = " + c);
+            $("#row").append(`<td>${r}</td>`);
+            //console.log("c = " + c);
             //console.log($("#row" + r))
         };
     };
